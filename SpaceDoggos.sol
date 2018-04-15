46106394155bc7c99ae67d34c86e005d3e6324b6
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-04-14 21:07:22 -0300

    Arithmetic operations and calculation of the Planet Code Modulus and the System Code Modulus

--- SpaceDoggos.sol
+++ SpaceDoggos.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.4.20;
 contract SpaceDoggos {
     uint maxPlanetsPerSystem = 10;
     uint minPlanetsPerSystem = 3;
-    
+
     uint planetCodeDigits = 7;
     uint systemCodeDigits = 7;
+
+    uint planetCodeModulus = 10 ** planetCodeDigits;
+    uint systemCodeModulus = 10 ** systemCodeDigits;
 }
