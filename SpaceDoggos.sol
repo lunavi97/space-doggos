61a8867df10e99c22721ad72b5ffd314a84a1f4a
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-04-14 21:01:07 -0300

    Initial variables definition

--- SpaceDoggos.sol
+++ SpaceDoggos.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.20;
 
 contract SpaceDoggos {
+    uint maxPlanetsPerSystem = 10;
+    uint minPlanetsPerSystem = 3;
     
+    uint planetCodeDigits = 7;
+    uint systemCodeDigits = 7;
 }
